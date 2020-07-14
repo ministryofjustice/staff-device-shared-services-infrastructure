7c404df737a330a32577f241b72d0f53edd8e7e2
Emile <emile.swarts@digital.justice.gov.uk>
2020-07-14 19:03:59 +0100

    Ship more required logs to OST
    This includes VPC Flowlogs and CloudTrail logs.

--- modules/log-forwarding/main.tf
+++ modules/log-forwarding/main.tf
@@ -1,13 +1,41 @@
-resource "aws_cloudwatch_log_subscription_filter" "cloud-watch-forward-subscription-filter-pttp-ci" {
+resource "aws_cloudwatch_log_subscription_filter" "cloud_watch_forward_subscription_filter_pttp_ci" {
   name            = "cloud-watch-forward-subscription-filter-pttp-ci"
   log_group_name  = "pttp-ci-infrastructure-log-group-core"
   filter_pattern  = ""
   destination_arn = var.destination_arn
 }
 
-resource "aws_cloudwatch_log_subscription_filter" "cloud-watch-forward-subscription-filter-sop-oci-access" {
+resource "aws_cloudwatch_log_subscription_filter" "cloud_watch_forward_subscription_filter_sop_oci_access" {
   name            = "cloud-watch-forward-subscription-filter-sop-oci-access"
   log_group_name  = "SOP-OCI-Access-codepipeline-log-group"
+  filter_pattern  = ""
+  destination_arn = var.destination_arn
+}
+
+resource "aws_cloudwatch_log_subscription_filter" "cloud_watch_forward_subscription_filter_global_protect_code_pipeline" {
+  name            = "cloud-watch-forward-subscription-filter-cloudtrail-infra"
+  log_group_name  = "GlobalProtect-codepipeline-log-group"
+  filter_pattern  = ""
+  destination_arn = var.destination_arn
+}
+
+resource "aws_cloudwatch_log_subscription_filter" "cloud_watch_forward_subscription_global_protect_logs" {
+  name            = "cloud-watch-forward-subscription-filter-global-protect-log-group"
+  log_group_name  = "GlobalProtect-log-group"
+  filter_pattern  = ""
+  destination_arn = var.destination_arn
+}
+
+resource "aws_cloudwatch_log_subscription_filter" "cloud_watch_forward_subscription_filter_panorama_codepipeline" {
+  name            = "cloud-watch-forward-subscription-filter-panorama-codepipeline-log-group"
+  log_group_name  = "Panorama-codepipeline-log-group"
+  filter_pattern  = ""
+  destination_arn = var.destination_arn
+}
+
+resource "aws_cloudwatch_log_subscription_filter" "cloud_watch_forward_subscription_filter_tgw_codepipeline" {
+  name            = "cloud-watch-forward-subscription-filter-tgw-codepipeline-log-group"
+  log_group_name  = "TGW-codepipeline-log-group"
   filter_pattern  = ""
   destination_arn = var.destination_arn
 }
