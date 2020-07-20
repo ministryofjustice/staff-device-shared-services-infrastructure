2c454cd39cdf8a6476ba2b25a3915576fe812d6c
Emile <emile.swarts@digital.justice.gov.uk>
2020-07-20 15:36:22 +0100

    Create script to set log retention periods to 7 days

---
+++ scripts/ensure_cloudwatch_log_retention_policies.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+log_group_names=$(aws logs describe-log-groups | jq -r '.logGroups | .[] | .logGroupName')
+retention_period=7
+
+for log_group_name in $log_group_names
+do
+  echo "setting log retention policy for $log_group_name to $retention_period"
+  aws logs delete-retention-policy --log-group-name $log_group_name
+  aws logs put-retention-policy --log-group-name $log_group_name --retention-in-days $retention_period
+done
