6026dbed54f4e95e97979fe820fa7a62314ad879
David Capper <david.capper@madetech.com>
2020-09-07 13:14:55 +0100

    Add pipeline for infrastructure monitoring and alerting

--- main.tf
+++ main.tf
@@ -152,6 +152,22 @@ module "pttp-infrastructure-ci-pipeline-dns-container" {
   privileged_mode = true
 }
 
+module "pttp-infrastructure-ci-pipeline-infra-monitoring-alerting" {
+  source                   = "./modules/ci-pipeline"
+  service_name             = "core"
+  github_organisation_name = "ministryofjustice"
+  github_repo_name         = "staff-infrastructure-monitoring"
+  git_branch_name          = "main"
+
+  prefix_name = "${module.label.id}-infra-monitoring-alerting"
+  vpc_id      = module.vpc.vpc_id
+  subnet_ids  = module.vpc.private_subnets
+
+  dev_assume_role_arn            = var.dev_assume_role_arn
+  pre_production_assume_role_arn = var.pre_production_assume_role_arn
+  production_assume_role_arn     = var.production_assume_role_arn
+}
+
 module "log-forward" {
   source          = "./modules/log-forwarding"
   destination_arn = var.kinesis_destination_arn
