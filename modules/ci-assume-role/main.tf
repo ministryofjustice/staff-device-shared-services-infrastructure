025cd5692ffb3a9027928434bc00b0b814bf7efa
David Capper <david.capper@madetech.com>
2020-09-08 14:06:29 +0100

    Add ec2 describeAvailabilityZones permission to assume role

--- modules/ci-assume-role/main.tf
+++ modules/ci-assume-role/main.tf
@@ -68,6 +68,7 @@ data "aws_iam_policy_document" "this" {
   statement {
     actions = [
       "ec2:CreateNetworkInterface",
+      "ec2:DescribeAvailabilityZones",
       "ec2:DescribeDhcpOptions",
       "ec2:DescribeNetworkInterfaces",
       "ec2:DeleteNetworkInterface",
