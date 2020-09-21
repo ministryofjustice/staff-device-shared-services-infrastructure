254865988445978595d538d140962f26635b2fd8
David Capper <david.capper@madetech.com>
2020-09-21 09:36:33 +0100

    Add pipeline for metric aggregation docker container

--- main.tf
+++ main.tf
@@ -157,6 +157,25 @@ module "pttp-infrastructure-ci-pipeline-dns-container" {
   privileged_mode = true
 }
 
+module "pttp-infrastructure-ci-pipeline-metric-aggregation-container" {
+  source                   = "./modules/ci-pipeline"
+  service_name             = ""
+  github_organisation_name = "ministryofjustice"
+  github_repo_name         = "staff-infrastructure-metric-aggregation-server"
+  git_branch_name          = "main"
+
+  name        = "${module.label.id}-metric-aggregation-server-pipeline"
+  prefix_name = "${module.label.id}-aggregation-server"
+  vpc_id      = module.vpc.vpc_id
+  subnet_ids  = module.vpc.private_subnets
+
+  dev_assume_role_arn            = var.dev_assume_role_arn
+  pre_production_assume_role_arn = var.pre_production_assume_role_arn
+  production_assume_role_arn     = var.production_assume_role_arn
+
+  privileged_mode = true
+}
+
 module "pttp-infrastructure-ci-pipeline-infra-monitoring-alerting" {
   source                   = "./modules/ci-pipeline"
   service_name             = "core"
