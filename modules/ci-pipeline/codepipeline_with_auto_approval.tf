558baf2a4c74cc67f5af9091887a59183bcc799a
Scott Edwards <scotteza@gmail.com>
2020-09-22 08:27:13 +0100

    Move CodePipeline resource to its own file

---
+++ modules/ci-pipeline/codepipeline_with_auto_approval.tf
@@ -0,0 +1,103 @@
+resource "aws_codepipeline" "codepipeline" {
+  name     = var.name
+  role_arn = aws_iam_role.codepipeline_role.arn
+
+  artifact_store {
+    location = aws_s3_bucket.artifacts.bucket
+    type     = "S3"
+
+    encryption_key {
+      id   = aws_kms_key.artifacts.arn
+      type = "KMS"
+    }
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub"
+      version          = "1"
+      output_artifacts = ["source_output"]
+
+      configuration = {
+        OAuthToken           = data.aws_ssm_parameter.github_oauth_token.value
+        Owner                = var.github_organisation_name
+        Repo                 = var.github_repo_name
+        Branch               = var.git_branch_name
+        PollForSourceChanges = "true"
+      }
+    }
+  }
+
+  stage {
+    name = "Test"
+
+    action {
+      name            = "Test"
+      category        = "Build"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      input_artifacts = ["source_output"]
+      version         = "1"
+
+      configuration = {
+        ProjectName = aws_codebuild_project.test.name
+      }
+    }
+  }
+
+  stage {
+    name = "Development"
+
+    action {
+      name            = "Deploy"
+      category        = "Build"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      input_artifacts = ["source_output"]
+      version         = "1"
+
+      configuration = {
+        ProjectName = aws_codebuild_project.development.name
+      }
+    }
+  }
+
+  stage {
+    name = "Pre_Production"
+
+    action {
+      name            = "Deploy"
+      category        = "Build"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      input_artifacts = ["source_output"]
+      version         = "1"
+
+      configuration = {
+        ProjectName = aws_codebuild_project.pre-production.name
+      }
+    }
+  }
+
+  stage {
+    name = "Production"
+
+    action {
+      name            = "Deploy"
+      category        = "Build"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      input_artifacts = ["source_output"]
+      version         = "1"
+
+      configuration = {
+        ProjectName = aws_codebuild_project.production.name
+      }
+    }
+  }
+}
