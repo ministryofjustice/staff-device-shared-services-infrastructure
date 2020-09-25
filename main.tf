ce4446ad63f041d2ab0ed8434b3c6712df46ef9c
Devonte <devo.tox.89@gmail.com>
2020-09-25 12:24:53 +0100

    Add Blackbox Exporter to CI Pipeline

--- main.tf
+++ main.tf
@@ -229,6 +229,26 @@ module "pttp-infrastructure-ci-pipeline-infra-monitoring-alerting-snmpexporter" 
   privileged_mode = true
 }
 
+module "staff-infrastructure-monitoring-blackbox-exporter" {
+  github_repo_name         = "staff-infrastructure-monitoring-blackbox-exporter"
+  source                   = "./modules/ci-pipeline"
+  github_organisation_name = "ministryofjustice"
+  git_branch_name          = "main"
+  service_name             = "core"
+
+  name        = "staff-infrastructure-monitoring-blackbox-exporter"
+  prefix_name = "${module.label.id}-bb-ex"
+
+  vpc_id      = module.vpc.vpc_id
+  subnet_ids  = module.vpc.private_subnets
+
+  dev_assume_role_arn            = var.dev_assume_role_arn
+  production_assume_role_arn     = var.production_assume_role_arn
+  pre_production_assume_role_arn = var.pre_production_assume_role_arn
+
+  privileged_mode = true
+}
+
 module "pttp-public-key-infrastructure-pipeline" {
   source                   = "./modules/ci-pipeline"
   service_name             = "core"
