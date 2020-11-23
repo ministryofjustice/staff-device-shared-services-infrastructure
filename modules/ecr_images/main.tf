9f9fae1108b19724afb5b090811d4f04c87e7dfc
Joshua-Luke Bevan <63861228+jbevan4@users.noreply.github.com>
2020-11-23 11:57:50 +0000

    Added policy to ecr so codepipeline can pull the images (#14)
    * Added policy to ecr so codepipeline can pull the images
    
    * Get correct value

--- modules/ecr_images/main.tf
+++ modules/ecr_images/main.tf
@@ -1,4 +1,43 @@
-resource "aws_ecr_repository" "this" {
-  for_each = var.repositories
-  name     = each.value
+resource "aws_ecr_repository" "docker_repository" {
+  for_each             = var.repositories
+  name                 = each.value
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository_policy" "docker_repository_policy" {
+  for_each   = var.repositories
+  repository = each.value
+
+  policy = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "1",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:PutImage",
+                "ecr:InitiateLayerUpload",
+                "ecr:UploadLayerPart",
+                "ecr:CompleteLayerUpload",
+                "ecr:DescribeRepositories",
+                "ecr:GetRepositoryPolicy",
+                "ecr:ListImages",
+                "ecr:DeleteRepository",
+                "ecr:BatchDeleteImage",
+                "ecr:SetRepositoryPolicy",
+                "ecr:DeleteRepositoryPolicy"
+            ]
+        }
+    ]
+}
+EOF
 }
