38e0eb2576ec9ddc8e9d37d891371370f3f921ac
Touhidul Islam <touhidul.islam@justice.gov.uk>
2021-04-13 16:00:32 +0100

    reuses ci-pipeline module for staff-device-private-dns-zone

--- main.tf
+++ main.tf
@@ -269,6 +269,26 @@ module "staff-infrastructure-monitoring-blackbox-exporter" {
   privileged_mode = true
 }
 
+module "staff-device-private-dns-zone" {
+  github_repo_name         = "staff-device-private-dns-zone"
+  source                   = "./modules/ci-pipeline"
+  github_organisation_name = "ministryofjustice"
+  git_branch_name          = "main"
+  service_name             = "core"
+
+  name        = "staff-device-private-dns-zone"
+  prefix_name = "${module.label.id}-pvt-dns"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  dev_assume_role_arn            = var.dev_assume_role_arn
+  production_assume_role_arn     = var.production_assume_role_arn
+  pre_production_assume_role_arn = var.pre_production_assume_role_arn
+
+  privileged_mode = true
+}
+
 module "log-forward" {
   source      = "./modules/log-forwarding"
   prefix_name = module.label.id
