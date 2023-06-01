6ed3fa701679072238dca7e61740c5cfa5431686
Stephen James <stephen.james4@digital.justice.gov.uk>
2023-06-01 15:58:21 +0100

    Move the terraform and provider block into a separate "versions.tf" file as per Terraform convention since v0.13

---
+++ versions.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket         = "pttp-global-bootstrap-pttp-infrastructure-tf-remote-state"
+    dynamodb_table = "pttp-global-bootstrap-pttp-infrastructure-terrafrom-remote-state-lock-dynamo"
+    key            = "terraform/v1/state"
+    region         = "eu-west-2"
+  }
+
+  required_providers {
+    aws = {
+      source                = "hashicorp/aws"
+      version               = "~> 3.0"
+      configuration_aliases = [aws.env]
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
