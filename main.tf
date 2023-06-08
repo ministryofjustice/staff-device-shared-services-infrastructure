d972d3ffc222241d07b23d228ec3dc04398d7546
Stephen James <stephen.james4@digital.justice.gov.uk>
2023-06-08 14:49:42 +0100

    Codestar connection applied
    to  "Staff-Device-Admin-Portal"

--- main.tf
+++ main.tf
@@ -123,16 +123,15 @@ module "staff-device-logging-syslog-to-cloudwatch-pipeline" {
 }
 
 module "pttp-infrastructure-ci-pipeline-dns-dhcp-admin-container" {
-  source                   = "./modules/ci-pipeline"
-  service_name             = "core"
-  github_organisation_name = "ministryofjustice"
-  github_repo_name         = "staff-device-dns-dhcp-admin"
-  git_branch_name          = "main"
-
-  name        = "Staff-Device-Admin-Portal"
-  prefix_name = "${module.label.id}-admin"
-  vpc_id      = module.vpc.vpc_id
-  subnet_ids  = module.vpc.private_subnets
+  source                  = "./modules/ci-pipeline-webhook"
+  service_name            = "core"
+  github_repo_id          = "ministryofjustice/staff-device-dns-dhcp-admin"
+  git_branch_name         = "main"
+  name                    = "Staff-Device-Admin-Portal"
+  prefix_name             = "${module.label.id}-admin"
+  codestar_connection_arn = aws_codestarconnections_connection.staff-infrastructure-moj.id
+  vpc_id                  = module.vpc.vpc_id
+  subnet_ids              = module.vpc.private_subnets
 
   dev_assume_role_arn            = var.dev_assume_role_arn
   pre_production_assume_role_arn = var.pre_production_assume_role_arn
