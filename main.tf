6a1b797f85b1e011257fb152fdbea143caba6836
Jahir.Uddin <jahir.uddin@digital.justice.gov.uk>
2023-06-12 14:54:10 +0100

    :recycle: Updated NAC server pipeline

--- main.tf
+++ main.tf
@@ -196,19 +196,18 @@ module "network-access-control-infrastructure" {
 }
 
 module "network-access-control-server" {
-  github_repo_name         = "network-access-control-server"
-  source                   = "./modules/ci-pipeline"
-  github_organisation_name = "ministryofjustice"
+  source                   = "./modules/ci-pipeline-webhook"
+  github_repo_id           = "ministryofjustice/network-access-control-server"
   git_branch_name          = "main"
   service_name             = "core"
   docker_image             = "aws/codebuild/standard:5.0"
   manual_production_deploy = true
 
-  name        = "network-access-control-server"
-  prefix_name = "${module.label.id}-nac-server"
-
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  name                    = "network-access-control-server"
+  prefix_name             = "${module.label.id}-nac-server"
+  codestar_connection_arn = aws_codestarconnections_connection.staff-infrastructure-moj.id
+  vpc_id                  = module.vpc.vpc_id
+  subnet_ids              = module.vpc.private_subnets
 
   dev_assume_role_arn            = var.dev_assume_role_arn
   production_assume_role_arn     = var.production_assume_role_arn
