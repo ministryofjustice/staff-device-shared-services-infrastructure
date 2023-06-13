5a79fdefe7ceafb3d1f2098ae18f2bd87eefe14e
Stephen James <stephen.james4@digital.justice.gov.uk>
2023-06-13 17:14:35 +0100

    Revert "Codestar connection applied"
    This reverts commit cc32b013fc71eed3eea2798a157431b497ead179.

--- main.tf
+++ main.tf
@@ -239,15 +239,17 @@ module "network-access-control-admin" {
 }
 
 module "staff-infrastructure-network-services" {
-  source                  = "./modules/ci-pipeline-webhook"
-  service_name            = "core"
-  github_repo_id          = "ministryofjustice/staff-infrastructure-network-services"
-  git_branch_name         = "main"
-  name                    = "staff-infrastructure-network-services"
-  prefix_name             = "${module.label.id}-net-svcs"
-  codestar_connection_arn = aws_codestarconnections_connection.staff-infrastructure-moj.id
-  vpc_id                  = module.vpc.vpc_id
-  subnet_ids              = module.vpc.private_subnets
+  source                   = "./modules/ci-pipeline"
+  service_name             = "core"
+  github_organisation_name = "ministryofjustice"
+  github_repo_name         = "staff-infrastructure-network-services"
+  git_branch_name          = "main"
+
+  name        = "staff-infrastructure-network-services"
+  prefix_name = "${module.label.id}-net-svcs"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
   dev_assume_role_arn            = var.dev_assume_role_arn
   pre_production_assume_role_arn = var.pre_production_assume_role_arn
