6fa285b814eaa82d95be0c3ba2d79feaed86fd7a
darey-io <dare.oluunmil@justice.gov.uk>
2023-06-14 15:43:27 +0100

    upgrade github version for staff-device-logging-syslog-to-cloudwatch

--- main.tf
+++ main.tf
@@ -103,16 +103,15 @@ module "pttp-infrastructure-ci-pipeline-dhcp-container" {
 
 # TODO this pipeline is in failed status, resolve before changing.
 module "staff-device-logging-syslog-to-cloudwatch-pipeline" {
-  source                   = "./modules/ci-pipeline"
-  service_name             = "core"
-  github_organisation_name = "ministryofjustice"
-  github_repo_name         = "staff-device-logging-syslog-to-cloudwatch"
-  git_branch_name          = "main"
-
-  name        = "Staff-Device-Logging-Syslog-To-Cloudwatch"
-  prefix_name = "${module.label.id}-log-syslog"
-  vpc_id      = module.vpc.vpc_id
-  subnet_ids  = module.vpc.private_subnets
+  source                  = "./modules/ci-pipeline-webhook"
+  service_name            = "core"
+  github_repo_id          = "ministryofjustice/staff-device-logging-syslog-to-cloudwatch"
+  git_branch_name         = "main"
+  name                    = "Staff-Device-Logging-Syslog-To-Cloudwatch"
+  prefix_name             = "${module.label.id}-log-syslog"
+  codestar_connection_arn = aws_codestarconnections_connection.staff-infrastructure-moj.id
+  vpc_id                  = module.vpc.vpc_id
+  subnet_ids              = module.vpc.private_subnets
 
   dev_assume_role_arn            = var.dev_assume_role_arn
   pre_production_assume_role_arn = var.pre_production_assume_role_arn
