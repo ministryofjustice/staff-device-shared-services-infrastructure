4a8ab07216cd90fb5a5f1406c694e6885ad0a69a
Jahir.Uddin <jahir.uddin@digital.justice.gov.uk>
2023-06-14 12:24:27 +0100

    :arrow_up: Upgraded github source action from 1 to 2

--- main.tf
+++ main.tf
@@ -217,19 +217,18 @@ module "network-access-control-server" {
 }
 
 module "network-access-control-admin" {
-  github_repo_name         = "network-access-control-admin"
-  source                   = "./modules/ci-pipeline"
-  github_organisation_name = "ministryofjustice"
+  source                   = "./modules/ci-pipeline-webhook"
+  github_repo_id           = "ministryofjustice/network-access-control-admin"
   git_branch_name          = "main"
   service_name             = "core"
   docker_image             = "aws/codebuild/standard:5.0"
   manual_production_deploy = true
 
-  name        = "network-access-control-admin"
-  prefix_name = "${module.label.id}-nac-admin"
-
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  name                    = "network-access-control-admin"
+  prefix_name             = "${module.label.id}-nac-admin"
+  codestar_connection_arn = aws_codestarconnections_connection.staff-infrastructure-moj.id
+  vpc_id                  = module.vpc.vpc_id
+  subnet_ids              = module.vpc.private_subnets
 
   dev_assume_role_arn            = var.dev_assume_role_arn
   production_assume_role_arn     = var.production_assume_role_arn
