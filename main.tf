210595f8025bc32fff0875b7fff284975340b85b
Stephen James <stephen.james4@digital.justice.gov.uk>
2023-07-21 12:45:57 +0100

    vpc and local moved to their files

--- main.tf
+++ main.tf
@@ -1,34 +1,5 @@
 data "aws_region" "current_region" {}
 
-locals {
-  cidr_block = "10.0.0.0/16"
-}
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "2.78.0"
-
-  name = module.label.id
-
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  cidr                 = local.cidr_block
-
-  azs = [
-    "${data.aws_region.current_region.id}a",
-    "${data.aws_region.current_region.id}b",
-    "${data.aws_region.current_region.id}c"
-  ]
-
-  private_subnets = [
-    cidrsubnet(local.cidr_block, 8, 1),
-    cidrsubnet(local.cidr_block, 8, 2),
-    cidrsubnet(local.cidr_block, 8, 3)
-  ]
-
-  map_public_ip_on_launch = false
-}
-
 module "label" {
   source  = "cloudposse/label/null"
   version = "0.25.0"
---
+++ vpc.tf
@@ -0,0 +1,24 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.78.0"
+
+  name = module.label.id
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  cidr                 = local.cidr_block
+
+  azs = [
+    "${data.aws_region.current_region.id}a",
+    "${data.aws_region.current_region.id}b",
+    "${data.aws_region.current_region.id}c"
+  ]
+
+  private_subnets = [
+    cidrsubnet(local.cidr_block, 8, 1),
+    cidrsubnet(local.cidr_block, 8, 2),
+    cidrsubnet(local.cidr_block, 8, 3)
+  ]
+
+  map_public_ip_on_launch = false
+}
