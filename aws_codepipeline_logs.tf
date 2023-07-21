4be1981016617e1737285b7dffd578d2cb10710e
Stephen James <stephen.james4@digital.justice.gov.uk>
2023-07-21 14:05:06 +0100

    Moved smtp codepipeline resources into own file

---
+++ aws_codepipeline_logs.tf
@@ -0,0 +1,34 @@
+module "pttp-infrastructure-ci-pipeline" {
+  source                   = "./modules/ci-pipeline-webhook"
+  service_name             = "core"
+  github_repo_id           = "ministryofjustice/staff-device-logging-infrastructure"
+  git_branch_name          = "main"
+  name                     = "Staff-Device-Logging"
+  prefix_name              = module.label.id
+  codestar_connection_arn  = aws_codestarconnections_connection.staff-infrastructure-moj.id
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  manual_production_deploy = true
+
+  dev_assume_role_arn            = var.dev_assume_role_arn
+  pre_production_assume_role_arn = var.pre_production_assume_role_arn
+  production_assume_role_arn     = var.production_assume_role_arn
+}
+
+module "staff-device-logging-syslog-to-cloudwatch-pipeline" {
+  source                  = "./modules/ci-pipeline-webhook"
+  service_name            = "core"
+  github_repo_id          = "ministryofjustice/staff-device-logging-syslog-to-cloudwatch"
+  git_branch_name         = "main"
+  name                    = "Staff-Device-Logging-Syslog-To-Cloudwatch"
+  prefix_name             = "${module.label.id}-log-syslog"
+  codestar_connection_arn = aws_codestarconnections_connection.staff-infrastructure-moj.id
+  vpc_id                  = module.vpc.vpc_id
+  subnet_ids              = module.vpc.private_subnets
+
+  dev_assume_role_arn            = var.dev_assume_role_arn
+  pre_production_assume_role_arn = var.pre_production_assume_role_arn
+  production_assume_role_arn     = var.production_assume_role_arn
+
+  privileged_mode = true
+}
---
+++ aws_codepipeline_smtp.tf
@@ -0,0 +1,33 @@
+module "staff-infrastructure-network-services" {
+  source                  = "./modules/ci-pipeline-webhook"
+  service_name            = "core"
+  github_repo_id          = "ministryofjustice/staff-infrastructure-network-services"
+  git_branch_name         = "main"
+  name                    = "staff-infrastructure-network-services"
+  prefix_name             = "${module.label.id}-net-svcs"
+  codestar_connection_arn = aws_codestarconnections_connection.nvvs-github-connection.id
+  vpc_id                  = module.vpc.vpc_id
+  subnet_ids              = module.vpc.private_subnets
+
+  dev_assume_role_arn            = var.dev_assume_role_arn
+  pre_production_assume_role_arn = var.pre_production_assume_role_arn
+  production_assume_role_arn     = var.production_assume_role_arn
+}
+
+module "staff-infrastructure-smtp-relay-server" {
+  source                  = "./modules/ci-pipeline-webhook"
+  service_name            = "core"
+  github_repo_id          = "ministryofjustice/staff-infrastructure-smtp-relay-server"
+  git_branch_name         = "main"
+  name                    = "staff-infrastructure-smtp-relay-server"
+  prefix_name             = "${module.label.id}-smtp-relay"
+  codestar_connection_arn = aws_codestarconnections_connection.staff-infrastructure-moj.id
+  vpc_id                  = module.vpc.vpc_id
+  subnet_ids              = module.vpc.private_subnets
+
+  dev_assume_role_arn            = var.dev_assume_role_arn
+  pre_production_assume_role_arn = var.pre_production_assume_role_arn
+  production_assume_role_arn     = var.production_assume_role_arn
+
+  privileged_mode = true
+}
