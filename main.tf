a07d00acc6d118501fa1f747c7d0e04a9dc50e6b
Stephen James <stephen.james4@digital.justice.gov.uk>
2023-07-21 12:53:08 +0100

    codestarconnections moved to their own file

--- main.tf
+++ main.tf
@@ -220,14 +220,3 @@ module "staff-infrastructure-smtp-relay-server" {
 
   privileged_mode = true
 }
-
-resource "aws_codestarconnections_connection" "nvvs-github-connection" {
-  name          = "nvvs github connection"
-  provider_type = "GitHub"
-  tags          = module.label.tags
-}
-resource "aws_codestarconnections_connection" "staff-infrastructure-moj" {
-  name          = "staff-infrastructure-moj"
-  provider_type = "GitHub"
-  tags          = module.label.tags
-}
