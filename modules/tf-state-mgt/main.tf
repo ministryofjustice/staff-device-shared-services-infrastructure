6462d2b94eb6336ca031d90c26e21d0218e7cb28
Stephen James <stephen.james4@digital.justice.gov.uk>
2023-07-25 13:48:48 +0100

    Mandatory tags added to tf-state-mgmt KMS Key
    Tags and aliases added.

--- modules/tf-state-mgt/main.tf
+++ modules/tf-state-mgt/main.tf
@@ -1,5 +1,14 @@
 resource "aws_kms_key" "artifacts" {
-  description = "${var.prefix_name}-artifacts-${var.service_name}"
+  description = "${var.prefix_name}-${var.service_name}-artifacts"
+
+  tags = merge(local.tags, {
+    Name = "${var.prefix_name}-${var.service_name}-artifacts"
+  })
+}
+
+resource "aws_kms_alias" "artifacts" {
+  name          = "alias/${var.prefix_name}-${var.service_name}-artifacts"
+  target_key_id = aws_kms_key.artifacts.id
 }
 
 locals {
--- modules/tf-state-mgt/parameter_store.tf
+++ modules/tf-state-mgt/parameter_store.tf
@@ -1,5 +1,13 @@
 resource "aws_kms_key" "peramater_store_key" {
   description             = "${var.prefix_name}-${var.service_name}-peramater-store-key"
   deletion_window_in_days = 10
-  tags                    = local.tags
+  
+  tags = merge(local.tags, {
+    Name = "${var.prefix_name}-${var.service_name}"
+  })
+}
+
+resource "aws_kms_alias" "peramater_store_key" {
+  name          = "alias/${var.prefix_name}-${var.service_name}"
+  target_key_id = aws_kms_key.peramater_store_key.id
 }
