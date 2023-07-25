24c97452b9bc526afbf455480a57b8fe0d719857
Stephen James <stephen.james4@digital.justice.gov.uk>
2023-07-25 14:50:44 +0100

    Manadaory tags applied to module dynamodb tables

--- modules/ci-pipeline-webhook/dynamodb.tf
+++ modules/ci-pipeline-webhook/dynamodb.tf
@@ -6,6 +6,10 @@ resource "aws_dynamodb_table" "dynamodb_terraform_state_lock" {
     name = "LockID"
     type = "S"
   }
+
+  tags = merge(local.tags, {
+    Name = "${var.prefix_name}-client-${var.service_name}-tf-lock-table"
+  })
 }
 
 resource "aws_appautoscaling_target" "dynamodb_table_read_target" {
