0cd6f2700d5f6c77176aade400a6c55489b563f9
Stephen James <stephen.james4@digital.justice.gov.uk>
2023-07-25 13:59:59 +0100

    Added SSM Parameter Store values to module
    for the tf-state-mgt module

--- modules/tf-state-mgt/variables.tf
+++ modules/tf-state-mgt/variables.tf
@@ -13,3 +13,15 @@ variable "tags" {
 variable "github_repo_id" {
   type = string
 }
+
+variable "dev_assume_role_arn" {
+  type = string
+}
+
+variable "pre_production_assume_role_arn" {
+  type = string
+}
+
+variable "production_assume_role_arn" {
+  type = string
+}
--- tf-state-mgt.tf
+++ tf-state-mgt.tf
@@ -4,4 +4,8 @@ module "staff-device-private-dns-zone" {
   prefix_name    = "${module.label.id}-pvt-dns"
   github_repo_id = "https://github.com/ministryofjustice/staff-device-private-dns-zone"
   tags           = module.label.tags
+
+  dev_assume_role_arn            = var.dev_assume_role_arn
+  pre_production_assume_role_arn = var.pre_production_assume_role_arn
+  production_assume_role_arn     = var.production_assume_role_arn
 }
