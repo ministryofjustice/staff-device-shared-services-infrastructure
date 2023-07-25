33646303e996c8bac5af4b4f4b7db3afc4e5a9d3
Stephen James <stephen.james4@digital.justice.gov.uk>
2023-07-25 14:34:12 +0100

    Refactored module ci-pipeline-webhook
    moved resources into logical groups of files for ease of understanding
    and updating.

---
+++ modules/ci-pipeline-webhook/s3-bucket.tf
@@ -0,0 +1,55 @@
+resource "aws_kms_key" "artifacts" {
+  description = "${var.prefix_name}-artifacts-${var.service_name}"
+}
+
+resource "aws_s3_bucket" "artifacts" {
+  bucket        = "${var.prefix_name}-build-artifact-bucket"
+  acl           = "private"
+  force_destroy = true
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.artifacts.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "artifacts" {
+  bucket = aws_s3_bucket.artifacts.bucket
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket" "client-tf-state" {
+  bucket        = "${var.prefix_name}-client-${var.service_name}-tf-state"
+  acl           = "private"
+  force_destroy = false
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.artifacts.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "client-tf-state" {
+  bucket = aws_s3_bucket.client-tf-state.bucket
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
