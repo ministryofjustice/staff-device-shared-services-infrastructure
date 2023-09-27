059038f461c8d58320078c24e2415282277ef50f
Stephen James <stephen.james4@digital.justice.gov.uk>
2023-09-27 20:12:57 +0100

    Remove deprecated terraform variables
    Variable values now stored and retrieved from AWS Secrets Manager.
    A "terraform.tfvars" file is no longer required.
    Avoids requirement of managing said file and enables GitHub Action usage
    without risk of exposing secrets in logs.

--- variables.tf
+++ variables.tf
@@ -8,34 +8,7 @@ variable "owner-email" {
   default = "lanwifi-devops@digital.justice.gov.uk"
 }
 
-variable "dev_assume_role_arn" {
-  type = string
-}
-
-variable "pre_production_assume_role_arn" {
-  type = string
-}
-
-variable "production_assume_role_arn" {
-  type = string
-}
-
-variable "production_account_id" {
-  type = string
-}
-
-variable "production_kinesis_destination_arn" {
-  type = string
-}
-
-variable "pre_production_kinesis_destination_arn" {
-  type = string
-}
-
-variable "development_kinesis_destination_arn" {
-  type = string
-}
-
 variable "enable_cloudtrail_log_shipping_to_cloudwatch" {
   type = bool
+  default = "true"
 }
