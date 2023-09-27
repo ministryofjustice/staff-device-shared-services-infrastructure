7f79f6c9a359949f59f1f49b17ae7538689aabf5
Stephen James <stephen.james4@digital.justice.gov.uk>
2023-09-27 20:10:07 +0100

    Assign secrets data source to locals

--- locals.tf
+++ locals.tf
@@ -2,6 +2,16 @@ locals {
   cidr_block = "10.0.0.0/16"
 
   ## vpc module requires the tags map without the "Name" value
-  ## It uses the "name" attribute internally and concatenates with other attributes 
+  ## It uses the "name" attribute internally and concatenates with other attributes
   tags_minus_name = { for k, v in module.label.tags : k => v if !contains(["Name"], k) }
+
+  dev_assume_role_arn            = nonsensitive(data.aws_secretsmanager_secret_version.dev_assume_role_arn.secret_string)
+  pre_production_assume_role_arn = nonsensitive(data.aws_secretsmanager_secret_version.pre_production_assume_role_arn.secret_string)
+  production_assume_role_arn     = nonsensitive(data.aws_secretsmanager_secret_version.production_assume_role_arn.secret_string)
+
+  production_kinesis_destination_arn     = nonsensitive(data.aws_secretsmanager_secret_version.production_kinesis_destination_arn.secret_string)
+  pre_production_kinesis_destination_arn = nonsensitive(data.aws_secretsmanager_secret_version.pre_production_kinesis_destination_arn.secret_string)
+  development_kinesis_destination_arn    = nonsensitive(data.aws_secretsmanager_secret_version.development_kinesis_destination_arn.secret_string)
+
+  production_account_id = nonsensitive(data.aws_secretsmanager_secret_version.production_account_id.secret_string)
 }
