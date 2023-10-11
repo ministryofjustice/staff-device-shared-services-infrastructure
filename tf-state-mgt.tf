9adbea848d08eb22711ee65a8c48de1fbee1043e
James Green <james.green6@digital.justice.gov.uk>
2023-10-11 17:23:48 +0100

    Added IAC for nvvs-devops-monitor
    Added IAC code for the nvvs-devops-monitor TF state bucket & TF dynamodb
    table.

--- tf-state-mgt.tf
+++ tf-state-mgt.tf
@@ -9,3 +9,15 @@ module "staff-device-private-dns-zone" {
   pre_production_assume_role_arn = local.pre_production_assume_role_arn
   production_assume_role_arn     = local.production_assume_role_arn
 }
+
+module "staff-devops-monitoring" {
+  source         = "./modules/tf-state-mgt"
+  service_name   = "monitoring-cluster"
+  prefix_name    = "${module.label_staff.id}"
+  github_repo_id = "https://github.com/ministryofjustice/nvvs-devops-monitor"
+  tags           = module.label_staff.tags
+
+  dev_assume_role_arn            = local.dev_assume_role_arn
+  pre_production_assume_role_arn = local.pre_production_assume_role_arn
+  production_assume_role_arn     = local.production_assume_role_arn
+}
