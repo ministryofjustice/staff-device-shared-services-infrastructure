dab34ba2c938d1b42cb30ecf4cf7344bb65aad29
Stephen James <stephen.james4@digital.justice.gov.uk>
2023-11-09 14:02:23 +0000

    Add an S3 bucket for file transfers. (#95)

---
+++ mojo_file_transfer.tf
@@ -0,0 +1,67 @@
+resource "aws_kms_key" "mojo_file_transfer" {
+  description = "mojo-file-transfer"
+
+  tags = merge(module.label_staff.tags, {
+    Name = "mojo-file-transfer"
+  })
+}
+
+resource "aws_kms_alias" "mojo_file_transfer" {
+  name          = "alias/mojo-file-transfer"
+  target_key_id = aws_kms_key.mojo_file_transfer.id
+}
+
+resource "aws_s3_bucket" "mojo_file_transfer" {
+  bucket        = "mojo-file-transfer"
+  force_destroy = false
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = merge(module.label_staff.tags, {
+    Name = "mojo-file-transfer"
+  })
+}
+
+resource "aws_s3_bucket_public_access_block" "mojo_file_transfer" {
+  bucket = aws_s3_bucket.mojo_file_transfer.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+resource "aws_s3_bucket_ownership_controls" "mojo_file_transfer" {
+  bucket = aws_s3_bucket.mojo_file_transfer.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "mojo_file_transfer" {
+  depends_on = [
+    aws_s3_bucket_public_access_block.mojo_file_transfer,
+    aws_s3_bucket_ownership_controls.mojo_file_transfer
+  ]
+
+  bucket = aws_s3_bucket.mojo_file_transfer.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "mojo_file_transfer" {
+  bucket = aws_s3_bucket.mojo_file_transfer.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "mojo_file_transfer" {
+  bucket = aws_s3_bucket.mojo_file_transfer.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mojo_file_transfer.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
