534014355022f081a39d63075ea5e1a7ba87c50f
Stephen James <stephen.james4@digital.justice.gov.uk>
2023-11-28 17:22:11 +0000

    Updated the get-secrets script
    Added menu to select the set of secrets required. This is to reduce noise and speed up retrieval.
    The Makefile target needed the interactive Docker setup.

--- scripts/get_secrets.sh
+++ scripts/get_secrets.sh
@@ -3,14 +3,88 @@
 set -e
 
 ## This script will retrieve the AWS Secrets Manager
-## Entries for this repository
 
-secrets=$(aws secretsmanager list-secrets --no-cli-pager \
---query "SecretList[].Name" \
---filters Key=tag-key,Values=application Key=tag-value,Values=pttp-shared-services-infrastructure \
---output json | jq '.[]' --raw-output)
+function nvvs-devops-monitor() {
+  echo ""
+	local tag_name="supported-application-secret-for"
+  local application_name="nvvs-devops-monitor"
 
-for secret in ${secrets}
-do
-  echo "${secret} -- $(aws secretsmanager get-secret-value --secret-id ${secret} --query "SecretString" --output text)"
-done
+  local tag_name2=supported-application-environment
+  local environment=${1}
+
+  secrets=$(aws secretsmanager list-secrets --no-cli-pager \
+  --query "SecretList[].Name" \
+  --filters Key=tag-key,Values=${tag_name} Key=tag-value,Values=${application_name} \
+  --filters Key=tag-key,Values=${tag_name2} Key=tag-value,Values=${environment} \
+  --output json | jq '.[]' --raw-output)
+
+	display_retreived_secrets "${secrets}" "${application_name}" "${environment}"
+}
+
+function pttp-shared-services-infrastructure() {
+  echo ""
+  local tag_name=application
+  local application_name="pttp-shared-services-infrastructure"
+
+  local secrets=$(aws secretsmanager list-secrets --no-cli-pager \
+  --query "SecretList[].Name" \
+  --filters Key=tag-key,Values=${tag_name} Key=tag-value,Values=${application_name} \
+  --output json | jq '.[]' --raw-output)
+
+	display_retreived_secrets "${secrets}" "${KEY}" "CI"
+}
+
+function display_retreived_secrets() {
+  local secrets=${1}
+  local application_name="${2}"
+  local environment="${3}"
+  echo ""
+	echo "Retreiving secrets for : application \"${application_name}\" environment \"${environment}\""
+	echo ""
+	for secret in ${secrets}
+  do
+    echo "${secret} -- $(aws secretsmanager get-secret-value --secret-id ${secret} --query "SecretString" --output text)"
+  done
+    echo ""
+}
+
+##
+# Color  Variables
+##
+green='\e[32m'
+blue='\e[34m'
+clear='\e[0m'
+
+##
+# Color Functions
+##
+
+ColorGreen(){
+	echo -ne $green$1$clear
+}
+ColorBlue(){
+	echo -ne $blue$1$clear
+}
+
+menu(){
+echo -ne "
+Get secrets for
+$(ColorGreen '1)') nvvs-devops-monitor development
+$(ColorGreen '2)') nvvs-devops-monitor pre-production
+$(ColorGreen '3)') nvvs-devops-monitor production
+$(ColorGreen '4)') pttp-shared-services-infrastructure
+$(ColorGreen '0)') Exit
+$(ColorBlue 'Choose an option:') "
+        read a
+        case $a in
+	        1) nvvs-devops-monitor "development" && nvvs-devops-monitor "shared";;
+	        2) nvvs-devops-monitor "pre-production" && nvvs-devops-monitor "shared";;
+	        3) nvvs-devops-monitor "production" && nvvs-devops-monitor "shared";;
+	        4) pttp-shared-services-infrastructure ;;
+		0) exit 0 ;;
+		*) echo -e $red"Wrong option."$clear; WrongCommand;;
+        esac
+}
+
+# Call the menu function
+menu
