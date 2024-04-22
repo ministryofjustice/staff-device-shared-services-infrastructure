68604c60b063a6f8f145f5f12aab46a144752773
lauren_tb <lauren.taylor-brown@justice.gov.uk>
2024-04-22 15:13:12 +0100

    Added manual steps to dns dhcp pipeline

--- aws_codepipeline_dns_dhcp.tf
+++ aws_codepipeline_dns_dhcp.tf
@@ -1,13 +1,15 @@
 module "pttp-infrastructure-ci-pipeline-dns-dhcp" {
-  source                  = "./modules/ci-pipeline-webhook"
-  service_name            = "core"
-  github_repo_id          = "ministryofjustice/staff-device-dns-dhcp-infrastructure"
-  git_branch_name         = "main"
-  name                    = "Staff-Device-DNS-DHCP-Infrastructure"
-  prefix_name             = "${module.label.id}-dns-dhcp"
-  codestar_connection_arn = aws_codestarconnections_connection.staff-infrastructure-moj.id
-  vpc_id                  = module.vpc.vpc_id
-  subnet_ids              = module.vpc.private_subnets
+  source                   = "./modules/ci-pipeline-webhook"
+  service_name             = "core"
+  github_repo_id           = "ministryofjustice/staff-device-dns-dhcp-infrastructure"
+  git_branch_name          = "main"
+  name                     = "Staff-Device-DNS-DHCP-Infrastructure"
+  prefix_name              = "${module.label.id}-dns-dhcp"
+  codestar_connection_arn  = aws_codestarconnections_connection.staff-infrastructure-moj.id
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  manual_production_deploy = true
+  production_plan          = true
 
   dev_assume_role_arn            = local.dev_assume_role_arn
   pre_production_assume_role_arn = local.pre_production_assume_role_arn
@@ -18,15 +20,17 @@ module "pttp-infrastructure-ci-pipeline-dns-dhcp" {
 }
 
 module "pttp-infrastructure-ci-pipeline-dns-container" {
-  source                  = "./modules/ci-pipeline-webhook"
-  service_name            = "core"
-  github_repo_id          = "ministryofjustice/staff-device-dns-server"
-  git_branch_name         = "main"
-  name                    = "Staff-Device-DNS-Server"
-  prefix_name             = "${module.label.id}-dns-server"
-  codestar_connection_arn = aws_codestarconnections_connection.staff-infrastructure-moj.id
-  vpc_id                  = module.vpc.vpc_id
-  subnet_ids              = module.vpc.private_subnets
+  source                   = "./modules/ci-pipeline-webhook"
+  service_name             = "core"
+  github_repo_id           = "ministryofjustice/staff-device-dns-server"
+  git_branch_name          = "main"
+  name                     = "Staff-Device-DNS-Server"
+  prefix_name              = "${module.label.id}-dns-server"
+  codestar_connection_arn  = aws_codestarconnections_connection.staff-infrastructure-moj.id
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  manual_production_deploy = true
+  production_plan          = true
 
   dev_assume_role_arn            = local.dev_assume_role_arn
   pre_production_assume_role_arn = local.pre_production_assume_role_arn
@@ -38,15 +42,17 @@ module "pttp-infrastructure-ci-pipeline-dns-container" {
 }
 
 module "pttp-infrastructure-ci-pipeline-dhcp-container" {
-  source                  = "./modules/ci-pipeline-webhook"
-  service_name            = "core"
-  github_repo_id          = "ministryofjustice/staff-device-dhcp-server"
-  git_branch_name         = "main"
-  name                    = "Staff-Device-DHCP-Server"
-  prefix_name             = "${module.label.id}-kea-server"
-  codestar_connection_arn = aws_codestarconnections_connection.staff-infrastructure-moj.id
-  vpc_id                  = module.vpc.vpc_id
-  subnet_ids              = module.vpc.private_subnets
+  source                   = "./modules/ci-pipeline-webhook"
+  service_name             = "core"
+  github_repo_id           = "ministryofjustice/staff-device-dhcp-server"
+  git_branch_name          = "main"
+  name                     = "Staff-Device-DHCP-Server"
+  prefix_name              = "${module.label.id}-kea-server"
+  codestar_connection_arn  = aws_codestarconnections_connection.staff-infrastructure-moj.id
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  manual_production_deploy = true
+  
 
   dev_assume_role_arn            = local.dev_assume_role_arn
   pre_production_assume_role_arn = local.pre_production_assume_role_arn
@@ -59,15 +65,17 @@ module "pttp-infrastructure-ci-pipeline-dhcp-container" {
 }
 
 module "pttp-infrastructure-ci-pipeline-dns-dhcp-admin-container" {
-  source                  = "./modules/ci-pipeline-webhook"
-  service_name            = "core"
-  github_repo_id          = "ministryofjustice/staff-device-dns-dhcp-admin"
-  git_branch_name         = "main"
-  name                    = "Staff-Device-Admin-Portal"
-  prefix_name             = "${module.label.id}-admin"
-  codestar_connection_arn = aws_codestarconnections_connection.staff-infrastructure-moj.id
-  vpc_id                  = module.vpc.vpc_id
-  subnet_ids              = module.vpc.private_subnets
+  source                   = "./modules/ci-pipeline-webhook"
+  service_name             = "core"
+  github_repo_id           = "ministryofjustice/staff-device-dns-dhcp-admin"
+  git_branch_name          = "main"
+  name                     = "Staff-Device-Admin-Portal"
+  prefix_name              = "${module.label.id}-admin"
+  codestar_connection_arn  = aws_codestarconnections_connection.staff-infrastructure-moj.id
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  manual_production_deploy = true
+
 
   dev_assume_role_arn            = local.dev_assume_role_arn
   pre_production_assume_role_arn = local.pre_production_assume_role_arn
